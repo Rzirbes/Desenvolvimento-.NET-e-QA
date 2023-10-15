33ea6c56be25d66174b6bc5bf9c1896412c9124a
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2023-10-15 10:29:11 -0300

    desafio POO

---
+++ trilha-net-poo-desafio-main/Models/Smartphone.cs
@@ -0,0 +1,33 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        protected Smartphone(string numero, string modelo, string iMEI, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+        }
+
+
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
+
+        
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("51-99999999", "Nokia", "123456-78901234", 14);
+Iphone iphone = new Iphone("51-88888888", "Iphone", "78901234-123456", 20);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("Joguinho");
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Meditação");
